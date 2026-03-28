51e2ffe13b4394f817446aa66299dd0dcf3d0767
Drew Bosley <dbosley@ncsu.edu>
2026-03-28 16:43:09 -0400

    Made the enemies actually stop

--- enemies/enemy.gd
+++ enemies/enemy.gd
@@ -8,19 +8,25 @@ class_name enemy
 @export var HP = 10 #I have no clue if this is a good number, 
 					#but it's a placeholder
 
-@onready var raycast: RayCast2D = $RayCast2D
+@onready var area: Area2D = $CollisionArea
 
 func _ready() -> void:
 	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if not raycast.is_colliding():
+	var areas = area.get_overlapping_areas()
+	var moving = true
+	for c: Area2D in areas:
+		if c.is_in_group("Creature"):
+			moving = false
+			#attack
+		elif c.is_in_group("Enemy"):
+			moving = false
+	
+	if moving:
 		position += Vector2.LEFT * speed * delta
-	else:
-		var collider = raycast.get_collider()
-		attack(collider)
-
+	
 func attack(collider: PhysicsBody2D):
 	pass
 
